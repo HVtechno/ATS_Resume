3c34ddf983bca6c819d1c19b6888c9e4bc3d6459
harihara ganesh <hganesh0786@gmail.com>
2025-05-07 14:15:08 +0200

    ATS_resume_V2.0

--- desktop.py
+++ desktop.py
@@ -1,6 +1,9 @@
 import threading
 import webview
 from app import app
+import os
+
+os.environ["QT_OPENGL"] = "software"
 
 def start_flask():
     print("Starting Flask server...")
@@ -11,5 +14,8 @@ if __name__ == '__main__':
     flask_thread.daemon = True
     flask_thread.start()
 
+    import time
+    time.sleep(1)
+
     webview.create_window("ATS Resume Analyzer", "http://127.0.0.1:5000", width=1000, height=800)
-    webview.start(gui='qt', http_server=True)
+    webview.start(gui='win32', http_server=True)
